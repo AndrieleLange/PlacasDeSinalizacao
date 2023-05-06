cfa0c881cabd619a7eac4ef7309ab2ab1ba3d44b
Andriele Lange <andrielebarcelange@gmail.com>
2023-05-06 15:30:13 -0300

    primeiro commit com códigos aux

---
+++ src/LeituraArquivo.java
@@ -0,0 +1,16 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LeituraArquivo {
+
+}
+
