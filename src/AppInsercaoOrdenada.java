893648f551e3bc0820f8fe74ce4acb7d541a779d
Andri Lange <andrielebarcelange@gmail.com>
2023-05-08 14:19:12 -0300

    Códigos de apoio e arqv csv

--- src/AppInsercaoOrdenada.java
+++ src/AppInsercaoOrdenada.java
@@ -1,3 +1,26 @@
+
 public class AppInsercaoOrdenada {
-   
+    public static void main(String[] args) {
+        LinkedListOfString lista = new LinkedListOfString();
+        lista.orderedAdd("ASSIS BRASIL");
+        lista.orderedAdd("ALBERTO BINS");
+        lista.orderedAdd("BENTO GONCALVES");
+        lista.orderedAdd("IPIRANGA");
+        lista.orderedAdd("ITABORAI");
+        lista.orderedAdd("SILVA SO");
+        lista.orderedAdd("ALBERTO BINS");
+        lista.orderedAdd("SALVADOR FRANCA");
+        lista.orderedAdd("CARLOS GOMES");
+        lista.orderedAdd("NILO PEÇANHA");
+        lista.orderedAdd("ALBERTO BINS");
+        lista.orderedAdd("ITABORAI");
+        lista.orderedAdd("BENTO GONCALVES");
+        lista.orderedAdd("MARIANTE");
+        lista.orderedAdd("ALBERTO BINS");
+        lista.orderedAdd("BENTO GONCALVES");
+        lista.orderedAdd("FELIPE DE OLIVEIRA");
+        lista.orderedAdd("LUCAS DE OLIVEIRA");
+        System.out.println(lista);
+
+    }
 }
