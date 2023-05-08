c41d7eef43159a9baac1841d1659bc62f8f76997
Andriele Barcé Lange <115933269+AndrieleLange@users.noreply.github.com>
2023-05-08 17:54:35 +0000

    iniciando os trabalhos

---
+++ src/Sinalizacao.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+
+public class Sinalizacao {
+    private String descricao;
+    private LocalDate implantacao;
+    private int numInicial;
+    private int numFinal;
+    //private ---- lado;
+    //private ---- localDeInstalacao;
+    public Sinalizacao(String descricao, LocalDate implantacao, int numInicial, int numFinal) {
+        this.descricao = descricao;
+        this.implantacao = implantacao;
+        this.numInicial = numInicial;
+        this.numFinal = numFinal;
+        //this.lado = lado;
+        //this.localDeInstalacao = localDeInstalacao;
+
+    }
+
+    
+
+}
