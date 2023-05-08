eb0c8733a43ff50d884ab0181aafc82243edfaf3
Andriele Barcé Lange <115933269+AndrieleLange@users.noreply.github.com>
2023-05-08 18:09:01 +0000

    trabalhando...

--- src/Sinalizacao.java
+++ src/Sinalizacao.java
@@ -5,6 +5,7 @@ public class Sinalizacao {
     private LocalDate implantacao;
     private int numInicial;
     private int numFinal;
+    private int count = 0;
     //private ---- lado;
     //private ---- localDeInstalacao;
     public Sinalizacao(String descricao, LocalDate implantacao, int numInicial, int numFinal) {
@@ -14,7 +15,7 @@ public class Sinalizacao {
         this.numFinal = numFinal;
         //this.lado = lado;
         //this.localDeInstalacao = localDeInstalacao;
-
+        count++;
     }
 
     public class ListaDeSinalizacoes {
@@ -23,6 +24,51 @@ public class Sinalizacao {
             public Node next;
         }
 
-    
+
+    /**
+     * // Métodos
+	add(sinalização)
+    */
+	public int size(){// retorna o total de sinalizações
+        return count;
+    } 
+
+    /**
+	int getMes(index) // retorna o mês de implantação da iésima sinalização 
+	LocalDate getDataImplantacao(index) // retorna data de implantação da iésima 
+//sinalização, para depois comparar qual 
+// é maior ou menor
+	getMenorData() // retorna a data da primeira sinalização instalada 
+// (considerando esta lista)
+	getMaiorData() // retorna a data da última sinalização instalada 
+// (considerando esta lista)
+	
+reset()
+	next()
+}
+// Obs: Poderia ter uma classe Rua
+public class ListaDeRuas {
+	private class Node {	
+		public Node prev
+		public Node next
+		public ListaDeSinalizacoes lista
+		public String nomeDaRua
+		public String idDaRua (Av, Rua, Trav, ...)
+	}
+	reset()
+	next()
+	prev()
+	String getRuaComMaisSinalizacoes ()
+	insereOrdenado(idDaRua, nome da rua, Sinalizacao) // procura pela rua, se já tem 
+				// insere a nova sinalização; se não tem, cria um novo nodo
+	contains(nome da rua)	
+	toString
+}
+
+public class App { // Poderiam ser duas classes
+	// Leitura do arquivo e a colocação das informações na lista
+	// Menu de opções
+}
+     */
 
 }
