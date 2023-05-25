a101ec07a9ed6a8a5c18fd84157d38b3c5ec9ed4
Andriele Barcé Lange <115933269+AndrieleLange@users.noreply.github.com>
2023-05-25 16:56:03 -0300

    concluido

--- src/AppInsercaoOrdenada.java
+++
@@ -1,9 +0,0 @@
-
-public class AppInsercaoOrdenada {
-    public static void main(String[] args) {
-        LinkedListOfString lista = new LinkedListOfString();
-        lista.orderedAdd(null);
-        System.out.println(lista);
-
-    }
-}
